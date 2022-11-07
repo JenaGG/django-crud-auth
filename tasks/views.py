907215c59af02e437983859a445e1cc09e326791
JenaGG <g_damian_martinez@hotmail.com>
2022-11-06 19:20:03 -0800

    a

---
+++ tasks/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def hello(request):
+    return render(request, 'signup.html')
